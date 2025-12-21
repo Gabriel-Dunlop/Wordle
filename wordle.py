cf63189c9883ac51c4aada10eb7d19714d6102a3
[Gabriel-Dunlop] <dunlop.gabriel@gmail.com>
2025-12-21 16:05:55 -0500

    update assignment

--- wordle.py
+++ wordle.py
@@ -13,28 +13,29 @@ def readFromFile():
             wordList.append(word.strip("\n"))
     word = (wordList[random.randint(0, len(wordList)-1)])
     return word
+
+def board():
+    board =["#####","#####","#####","#####","#####","#####"]
+    # display_board = '\n'.join(str(line)for line in board)
+    return board
+    
 # randomWord = readFromFile()
 # userInput(randomWord)
-
+testing = board()
 test = "test"
 userWord = "spank"
 randomWord = "prank"
 emptyString = ""
-
+j = 0
 for i in range(0, len(userWord)):
     if userWord[i] == randomWord[i]:
-        # board[i].replace()
+        testing[j][i] == randomWord[i]
+        print(testing[j][i])
+        j += 1
+        
         emptyString += randomWord[i]
         continue      
     if userWord[i] in randomWord:
         print("it contains!")
     if userWord[i] not in randomWord:
         print("does not contain!")
-
-def board():
-    board =["#####","#####","#####","#####","#####","#####"]
-    display_board = '\n'.join(str(line)for line in board)
-    return display_board
-    
-testing = board()
-print(testing)
